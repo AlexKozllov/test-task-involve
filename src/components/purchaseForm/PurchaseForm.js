e4a7ef76de78cd726f07ec68d368756296b34bcd
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2021-05-18 01:17:38 +0300

    added some styles for PurchaseForm

--- src/components/purchaseForm/PurchaseForm.js
+++ src/components/purchaseForm/PurchaseForm.js
@@ -1,24 +1,55 @@
 import React from "react";
+import { SelectLabel } from "./styleForm";
+import AsyncSelect from "react-select/async";
+import Select from "react-select";
+import Loader from "../loader/Loader";
 
 const PurchaseForm = ({ payment, methods }) => {
   return (
-    <label>
-      {payment}
-      {/* <select value={this.state.value} onChange={this.handleChange}> */}
-      <select>
-        {methods.map((item) => {
-          return (
-            <option key={item.id} value={item.name}>
-              {item.name}
-            </option>
-          );
-        })}
-        {/* <option value="grapefruit">Грейпфрут</option>
-            <option value="lime">Лайм</option>
-            <option value="coconut">Кокос</option>
-            <option value="mango">Манго</option> */}
-      </select>
-    </label>
+    // <SelectLabel>
+    //   {payment}
+    //   {/* <select value={this.state.value} onChange={this.handleChange}> */}
+    //   <select>
+    //     {methods.map((item) => {
+    //       return (
+    //         <option key={item.id} value={item.name}>
+    //           {item.name}
+    //         </option>
+    //       );
+    //     })}
+    //   </select>
+    // </SelectLabel>
+    // <AsyncSelect
+    //   cacheOptions
+    //   loadOptions={methods}
+    //   defaultOptions
+    //   //   onInputChange={this.handleInputChange}
+    // />
+    <SelectLabel
+      className="basic-single"
+      classNamePrefix="select"
+      //   defaultValue={methods[1]}
+      //   isDisabled={isDisabled}
+      isLoading={false}
+      isClearable={false}
+      isSearchable={true}
+      name="color"
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          color: "red",
+          primary25: "#F4F4F4",
+          primary: "blue",
+        },
+      })}
+      options={methods.reduce((acc, item) => {
+        acc.push({ value: item.id, label: item.name });
+        console.log(acc);
+        return acc;
+      }, [])}
+    />
   );
 };
 
---
+++ src/components/purchaseForm/styleForm.js
@@ -0,0 +1,29 @@
+import Select from "react-select";
+import styled from "styled-components";
+
+const SelectLabel = styled(Select)`
+  font-family: Roboto;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 56px;
+  letter-spacing: 0em;
+  text-align: left;
+
+  select {
+    font-family: Roboto;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+
+    &:focus-visible::hover {
+      background-color: #f4f4f4;
+      color: red;
+    }
+  }
+`;
+
+export { SelectLabel };
